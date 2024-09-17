6d3394a34444b5072151799f4e6413ea73b5efa8
subirdas29 <subirdas1045@gmail.com>
2024-09-17 23:46:10 +0600

    server added

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ const allPost = async()=>{
     postHandle(data)
 }
 
+allPost()
 
 // Show post
 
-const postHandle = posts =>{
+const postHandle = post =>{
     const postShow = document.getElementById('post')
 
-    const post = posts.slice(0,20)
+    // const post = posts.slice(0,20)
 
     console.log(post.length);
     
@@ -38,12 +39,12 @@ const postHandle = posts =>{
 
 // create post
 
-const createPost = ()=>{
+const createPost = (title,textAreas)=>{
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: JSON.stringify({
-          title: 'foo',
-          body: 'bar',
+          title: title,
+          body: textAreas,
           userId: 1,
         }),
         headers: {
@@ -51,11 +52,54 @@ const createPost = ()=>{
         },
       })
         .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then(data => console.log(data));
 }
 
 
-allPost()
+const allPost1st = async(userId)=>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${userId}`)
+    const data = await res.json()
+    postadded(data)
+    // console.log(data);
+    
+    
+}
+
+
+const postadded = data =>{
+    const postShow = document.getElementById('post')
+
+    // const post = posts.slice(0,20)
+
+    console.log(post.length);
+    
+    // post.forEach(post=>{
+        const allPost = document.createElement('p')
+
+    allPost.innerHTML = `
+                <div class="card-body">
+                  <h2 class="card-title">${data.title}</h2>
+                  <p>${data.body}</p>
+                  
+                </div>
+    `
+    postShow.appendChild(allPost)
+    
+    console.log(post)
+    // })
+
+}
+
+
+
+const postText=()=>{
+    const title = document.getElementById('title')
+    const textArea = document.getElementById('textarea')
+    const textTile = title.value
+    const textAreas = textArea.value
+    createPost(textTile,textAreas)
+
+}
 
 {/* <div class="card-actions justify-center">
                     <button class="btn btn-primary" onclick="showDetails('${phone.slug}')">Show Details</button>
