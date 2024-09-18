e836e73651dc2297fc38c7e26257d1560ae7cc90
subirdas29 <subirdas1045@gmail.com>
2024-09-18 21:18:03 +0600

    delete added

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ allPost()
 const showPost = (posts) =>{
   const postText = document.getElementById('post')
 
+
   posts.forEach(post =>{
     
     const div = document.createElement('div')
+    
+
     div.innerHTML = `
     <h1 class="text-2xl font-bold">${post.title}</h1>
     <p>${post.body}</p>
     <div class="mt-6 mb-12">
       <button class="text-blue-400 mr-10" onclick="editPosts('${post.id}')">Edit</button>
-    <button class="text-blue-400">Delete</button>
+    <button class="text-blue-400" onclick="deletePost('${post.id}')">Delete</button>
     </div>
     `
     postText.appendChild(div)
@@ -68,45 +71,47 @@ const showPost = (posts) =>{
 
 const editPosts = async (id)=>{
   const edit = document.getElementById('editpost')
+  const btn = document.getElementById('btn')
   const editTitle = document.getElementById('editTitle')
   const editBody = document.getElementById('editBody')
-  const editSubmit = document.getElementById('editSubmit')
+
+  const res = await fetch(`http://localhost:3000/posts/${id}`)
+  const post = await res.json()
+
+  editTitle.value = post.title
+  editBody.value = post.body
+
   console.log(id)
   edit.showModal()
 
-  const newEditTitle = editTitle.value
-  const newEditbody = editBody.value
+  btn.innerHTML = `
   
-
-  edit.innerHTML = `
-  <div class="modal-box flex flex-col items-center">
-        <input class="text-lg font-bold input input-bordered input-secondary  w-full my-8 max-w-xs" id="editTitle"></input>
-        <textarea class="py-4 input input-bordered input-secondary  w-full max-w-xs mb-8" id="editBody"></textarea>
-        
-        <div class="flex justify-center items-center">
-            <button class="btn mr-6" onclick="newPostEdit()" id="editSubmit" >Submit</button>
+            <button class="btn mr-6" onclick="newPostEdit('${id}')" >Submit</button>
             <div class="modal-action mt-0">
                 <form method="dialog">
-                  <!-- if there is a button in form, it will close the modal -->
-                 
                   <button class="btn">Close</button>
-                 
                 </form>
-              </div>
+              
         </div>
-      </div>
+     
   `
-
-
-  
-
-  // editSubmit.onclick(newPostEdit(newEditTitle,newEditbody,id))
-  // newPostEdit(newEditTitle,newEditbody,id)
-  
 }
 
 
-const newPostEdit = async(newEditTitle,newEditbody,id) =>{
+const newPostEdit = async(id) =>{
+  console.log(id)
+  
+
+
+
+  const editTitle = document.getElementById('editTitle')
+  const editBody = document.getElementById('editBody')
+
+  let newEditTitle = editTitle.value
+  let newEditbody = editBody.value
+  
+
+  console.log(newEditTitle,newEditbody)
 
   const res = await fetch(`http://localhost:3000/posts/${id}`, {
     method: 'PUT',
@@ -122,4 +127,15 @@ const newPostEdit = async(newEditTitle,newEditbody,id) =>{
 
     const data = await res.json()
     console.log(data)
+}
+
+
+//Delete Post
+
+
+const deletePost = (id)=>{
+  
+  fetch(`http://localhost:3000/posts/${id}`, {
+    method: 'DELETE',
+  });
 }
