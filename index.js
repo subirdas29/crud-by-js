219490672c7eb419ba4b50e01adca7f2484bd89c
subirdas29 <subirdas1045@gmail.com>
2024-09-18 20:08:32 +0600

    edit added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const showPost = (posts) =>{
     <h1 class="text-2xl font-bold">${post.title}</h1>
     <p>${post.body}</p>
     <div class="mt-6 mb-12">
-      <button class="text-blue-400 mr-10">Edit</button>
+      <button class="text-blue-400 mr-10" onclick="editPosts('${post.id}')">Edit</button>
     <button class="text-blue-400">Delete</button>
     </div>
     `
@@ -61,3 +61,65 @@ const showPost = (posts) =>{
 
   newPost(addTitle,addTextArea)
  }
+
+
+
+//  Edit Post
+
+const editPosts = async (id)=>{
+  const edit = document.getElementById('editpost')
+  const editTitle = document.getElementById('editTitle')
+  const editBody = document.getElementById('editBody')
+  const editSubmit = document.getElementById('editSubmit')
+  console.log(id)
+  edit.showModal()
+
+  const newEditTitle = editTitle.value
+  const newEditbody = editBody.value
+  
+
+  edit.innerHTML = `
+  <div class="modal-box flex flex-col items-center">
+        <input class="text-lg font-bold input input-bordered input-secondary  w-full my-8 max-w-xs" id="editTitle"></input>
+        <textarea class="py-4 input input-bordered input-secondary  w-full max-w-xs mb-8" id="editBody"></textarea>
+        
+        <div class="flex justify-center items-center">
+            <button class="btn mr-6" onclick="newPostEdit()" id="editSubmit" >Submit</button>
+            <div class="modal-action mt-0">
+                <form method="dialog">
+                  <!-- if there is a button in form, it will close the modal -->
+                 
+                  <button class="btn">Close</button>
+                 
+                </form>
+              </div>
+        </div>
+      </div>
+  `
+
+
+  
+
+  // editSubmit.onclick(newPostEdit(newEditTitle,newEditbody,id))
+  // newPostEdit(newEditTitle,newEditbody,id)
+  
+}
+
+
+const newPostEdit = async(newEditTitle,newEditbody,id) =>{
+
+  const res = await fetch(`http://localhost:3000/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title: newEditTitle,
+      body: newEditbody,
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+
+    const data = await res.json()
+    console.log(data)
+}
